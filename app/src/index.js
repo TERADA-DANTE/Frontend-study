85168c3915139d44413d05e7f642307a39e24b5a
TERADA-DANTE <terada.dante@gmail.com>
2020-12-15 15:22:39 +0900

    Install npm antd

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App';
+import './App.less';
 
 ReactDOM.render(
   <React.StrictMode>
