377b9f757a98090a50589dd8d9b87a0eb07b919d
TERADA-DANTE <terada.dante@gmail.com>
2020-12-15 16:26:20 +0900

    Install @craco/craco

--- app/src/Components/App.js
+++ app/src/Components/App.js
@@ -1,23 +1,8 @@
-import './App.css';
+import "./App.css"
 
+import General from "./Templates/General/index"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <General></General>
 }
 
-export default App;
+export default App
